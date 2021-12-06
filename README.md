23168f95a6d7bc4158368c35d7f182bdaf25e0fe
Breathtake <breftejk@gmail.com>
2021-12-06 18:42:17 +0100

    v1.0.2

---
+++ README.md
@@ -0,0 +1,39 @@
+# DISCORD-COMMAND-COOLDOWN
+
+NPM: https://www.npmjs.com/package/discord-command-cooldown
+<br>
+GitHub: https://github.com/breftejk/discord-command-cooldown
+<br>
+Discord Support Server: https://discord.gg/CzfMGtrdaA
+
+<hr>
+
+A module that allows you to detect if a command should have a cooldown imposed on it and not run for the user. We are above others thanks to the fact that even after restart cooldown is still taken into account according to timeout that was set from the beginning. For example, you set the cooldown for the user to 30 minutes, the bot will be off for 10 minutes, then if you turn it on again, the module will know that there are 20 minutes left!
+
+# Installation
+
+```js
+npm i discord-command-cooldown
+```
+
+# Usage
+
+```js
+const {CommandCooldown, msToMinutes} = require('discord-command-cooldown');
+
+const earnCashCommandCooldown = new CommandCooldow('earnCash', 60000); // Allow users to execute this command each 60 seconds
+
+...
+if(command == `${prefix}earn`){
+    const userCooldowned = await earnCashCommandCooldown.getUser(message.author.id); // Check if user need to be cooldowned
+    if(userCooldowned){
+        const timeLeft = msToMinutes(userAllowed.msLeft);
+        return message.reply(`You need to wait ${timeLeft.minutes + ' minutes and ' + timeLeft.seconds + ' seconds'} before running command again!`);
+    }
+    
+    // your command stuff
+
+    await earnCashCommandCooldown.addUser(message.author.id); // Cooldown user again
+}
+...
+```
---
+++ index.js
@@ -0,0 +1,72 @@
+const fs = require('fs');
+const path = require('path');
+
+async function saveUser(userID, commandName, timeEnd){
+    const data = fs.readFileSync(path.resolve(__dirname,'activeTimeouts.json'));
+    let activeTimeouts = JSON.parse(data);
+    activeTimeouts[commandName] ? null : activeTimeouts[commandName] = {};
+    activeTimeouts[commandName][userID] = {userID, timeEnd};
+
+    fs.writeFileSync(path.resolve(__dirname,'activeTimeouts.json'), JSON.stringify(activeTimeouts, null, 3));
+}
+
+async function checkForUser(userID, commandName) {
+    const data = fs.readFileSync(path.resolve(__dirname,'activeTimeouts.json'));
+    let activeTimeouts = JSON.parse(data);
+
+    if(!activeTimeouts[commandName])return false;
+    if(!activeTimeouts[commandName][userID])return false;
+
+    const dateNow = Date.now();
+    const now = new Date(dateNow);
+
+    let userData = activeTimeouts[commandName][userID];
+
+    if(now > new Date(userData.timeEnd)){
+        delete activeTimeouts[commandName][userID];
+        fs.writeFileSync(path.resolve(__dirname,'activeTimeouts.json'), JSON.stringify(activeTimeouts, null, 3));
+        return false;
+    }
+
+    let nowDate = now;
+    let endDate = new Date(userData.timeEnd);
+    userData.msLeft = Math.abs(nowDate.getTime() - endDate.getTime()).toString();
+    return userData;
+}
+
+class CommandCooldown {
+    constructor(commandName, timeout) {
+        this.commandName = commandName;
+        this.timeout = timeout;
+        const data = fs.readFileSync(path.resolve(__dirname,'activeTimeouts.json'));
+        const activeTimeouts = JSON.parse(data);
+        this.activeTimeouts = activeTimeouts;
+        if(!activeTimeouts[commandName])activeTimeouts[commandName]={};
+        for (let v of Object.values(activeTimeouts[commandName])){
+            checkForUser(v.userID, this.commandName);
+        }
+    }
+
+    async addUser(userID) {
+        let endDate = Date.now();
+        endDate += this.timeout;
+        let timeEnd = new Date(endDate);
+
+        await saveUser(userID, this.commandName, timeEnd);
+    }
+
+    async getUser(userID) {
+        return checkForUser(userID, this.commandName);
+    }
+}
+
+module.exports = {
+    CommandCooldown,
+    msToMinutes: (ms) => {
+        ms = Number(ms);
+        const minutes = Math.floor((ms/1000/60) << 0),
+            seconds = Math.floor((ms/1000) % 60);
+
+        return { minutes, seconds };
+    }
+};
